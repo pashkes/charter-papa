afddbe67f5966f9999e2e3eebcdcbc0cc2a4b880
pashkes <pashkes@gmail.com>
2017-05-04 01:13:27 +0300

    Update 2017-05-03T22:13:15.272Z

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,26 +1,48 @@
-/**
- * pgwBrowser init
- */
-var pgwBrowser = $.pgwBrowser(),
-    isMobile = navigator.userAgent.match(/Mobile/i) == "Mobile",
-    ajaxUrl = "/wp-admin/admin-ajax.php",
-    site_cookie_domain = "."+document.location.hostname.replace("www.","");
-
+'use strict';
 /**
  * Document ready functions
  */
-(function ($) {
-    $(document).ready(function () {
-
-        /**
-         * Add os and browser classes for body
-         */
-        if(pgwBrowser.browser.group){
-            $("body").addClass(pgwBrowser.browser.group.replace(" ","").toLowerCase());
-        }
-        if(pgwBrowser.os.group){
-            $("body").addClass(pgwBrowser.os.group.replace(" ","").toLowerCase());
-        }
-
+$(function () {
+  if ($('.search').length) {
+    $('.js-search-extended').on('click', function () {
+      $('.search').toggleClass('search--show-extended');
     });
-})(jQuery);
+  }
+  $(".js-example-placeholder-multiple").select2({
+    placeholder: ""
+  });
+
+  $( "#slider-range" ).slider({
+    range: true,
+    min: 0,
+    max: 15000,
+    values: [ 4800, 7700 ],
+    slide: function( event, ui ) {
+      $( "#range" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
+    }
+  });
+  $( "#range" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ) +
+      " - $" + $( "#slider-range" ).slider( "values", 1 ) );
+
+
+});
+$(window).on('resize load', function () {
+  var imgHeight = $('.places__wrapper--monaco').outerHeight();
+  $('.places__item--center img').css('height', imgHeight)
+});
+
+//Sticky header
+var headerTop = $('.js-header-top');
+function bgChangeHeader() {
+  if (headerTop.length) {
+    if ($(window).scrollTop() > 1) {
+      headerTop.addClass("header__top--scroll");
+    }
+    else {
+      headerTop.removeClass("header__top--scroll");
+    }
+  }
+}
+$(window).on('scroll load', function () {
+  bgChangeHeader();
+});
