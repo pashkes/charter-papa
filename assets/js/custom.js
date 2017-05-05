f859958170e09f38c5bec8f64b0092d85cec724c
pashkes <pashkes@gmail.com>
2017-05-05 14:56:36 +0300

    Update 2017-05-05T11:56:33.998Z

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -7,31 +7,31 @@
 $(function () {
 
   var slider = $('.js-charters-slider.single-item');
-  var isFirst = true; // Флаг для проверки на предзагрузку
-
-  function setArrowsBackground() {
-    // $('.charters__arrow-wrapper--back'').animate({ opacity: 0 }, 100);
-    // $('.charters__arrow-wrapper--next').animate({ opacity: 0 }, 100);
-
-
-    var currentSlide = isFirst ? 0 : slider.slick('slickCurrentSlide');
-    isFirst = false; // Сеттим в false
-    var slides = slider.find('li:not(.slick-cloned) .charters__img img');
-    var prevSlide = currentSlide === 0 ? slides.length - 1 : currentSlide - 1;
-    var nextSlide = currentSlide === slides.length - 1 ? 0 : currentSlide + 1;
-    var s = slider.slick('getSlick');
-    var next = slides[nextSlide].currentSrc;
-    var prev = slides[prevSlide].currentSrc;
-    console.log(next);
-    console.log(prev);
-    $('.charters__arrow-wrapper--back').css('backgroundImage', 'url("' + prev + '")');
-    $('.charters__arrow-wrapper--next').css('backgroundImage', 'url("' + next + '")');
-
-    // $('.charters__arrow-wrapper--back').animate({ opacity: 1 }, 600);
-    // $('.charters__arrow-wrapper--next').animate({ opacity: 1 }, 600);
-  }
-
-  setTimeout(setArrowsBackground, 0);
+  // var isFirst = true; // Флаг для проверки на предзагрузку
+  //
+  // function setArrowsBackground() {
+  //   // $('.charters__arrow-wrapper--back'').animate({ opacity: 0 }, 100);
+  //   // $('.charters__arrow-wrapper--next').animate({ opacity: 0 }, 100);
+  //
+  //
+  //   var currentSlide = isFirst ? 0 : slider.slick('slickCurrentSlide');
+  //   isFirst = false; // Сеттим в false
+  //   var slides = slider.find('li:not(.slick-cloned) .charters__img img');
+  //   var prevSlide = currentSlide === 0 ? slides.length - 1 : currentSlide - 1;
+  //   var nextSlide = currentSlide === slides.length - 1 ? 0 : currentSlide + 1;
+  //   var s = slider.slick('getSlick');
+  //   var next = slides[nextSlide].currentSrc;
+  //   var prev = slides[prevSlide].currentSrc;
+  //   console.log(next);
+  //   console.log(prev);
+  //   $('.charters__arrow-wrapper--back').css('backgroundImage', 'url("' + prev + '")');
+  //   $('.charters__arrow-wrapper--next').css('backgroundImage', 'url("' + next + '")');
+  //
+  //   // $('.charters__arrow-wrapper--back').animate({ opacity: 1 }, 600);
+  //   // $('.charters__arrow-wrapper--next').animate({ opacity: 1 }, 600);
+  // }
+  //
+  // setTimeout(setArrowsBackground, 0);
 
   //slider Header home page
   if ($('.is-home').length) {
